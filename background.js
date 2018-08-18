75ea0aab1d7021899652e2090ea47c24c8d946d2
Ryan Michael Lahaie <lahaiery@msu.edu>
2018-08-18 11:30:01 -0400

    Added images directory and updated extension icons.

---
+++ background.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({ color: '#3aa757' }, function () {
+        console.log("The color is green.");
+    });
+});
