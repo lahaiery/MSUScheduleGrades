fdd17c0c51138d9895c35c78f91f5020228f30c1
Ryan Michael Lahaie <lahaiery@msu.edu>
2018-09-07 17:03:03 -0400

    Added support for direct links from average/median gpa to the associated MSUgrades page.

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 // JavaScript source code
 
-var baseMSUGradesURL = "https://msugrades.com/course/"; //Base URL for all MSUgrades.com AJAX requests
-
+const BASEMSUGRADESURL = "https://msugrades.com"; //Base URL for all MSUgrades.com AJAX requests
 
 //Parses Planner page for all professor names
 var professor = document.querySelectorAll('td[data-title="Instructor"]');
@@ -20,7 +19,7 @@ avg_th.id = "Th9";
 var header = document.querySelector("#MainContent_UCEnrl_rptPlanner_trHeader > tr");
 header.appendChild(avg_th);
 
-//Inserts an Avg. GPA column in the schedule builder table
+//Inserts a Median GPA column in the schedule builder table
 var median_th  = document.createElement("th");
 //Sets the inner HTML of the newly created th element
 median_th.innerHTML = 'Median GPA'
@@ -54,10 +53,11 @@ for(i = 0; i < course.length; i++)
     var lastName = nameSplit[1].trim();
     let avgGpa = "N/A";
     let medianGpa = "N/A";
+    let detailedLink = "";
 
     //Create an array for the course and the professor containing subject, course number and first name, last name
     var courseArr = [subject, courseNumber];
-    var nameArr = [firstName, lastName, avgGpa, medianGpa];
+    var nameArr = [firstName, lastName, avgGpa, medianGpa, detailedLink];
     
     //Adds the two arrays to a map, course ID is the key, professor name is value
     scheduleMap.set(courseArr, nameArr);
@@ -72,7 +72,7 @@ for (let [key] of scheduleMap.entries()) {
         processRequest(httpRequest, key);// we're calling our method
     };
 
-    httpRequest.open('GET', baseMSUGradesURL + key[0] + "/" + key[1], true);
+    httpRequest.open('GET', BASEMSUGRADESURL + "/course" + "/" + key[0] + "/" + key[1], true);
     httpRequest.send();
     ajaxCounter++;
 }
@@ -106,15 +106,20 @@ function processRequest(xhr, key) {
 
                 //Parses the average and median GPA from the second column of the table
                 var gradeMatches = container.querySelectorAll("p > em");
+
+                var linkMatches = container.querySelectorAll("p > a");
+
                 if(gradeMatches[0] !== undefined)
                 {
                     var avgGPA = gradeMatches[0].innerText;
                     var medianGPA = gradeMatches[1].innerText;
+                    var link = linkMatches[0].getAttribute("href");
 
                     if(professorName.toLowerCase().includes(scheduleMap.get(key)[1].toLowerCase()))
                     {
                         scheduleMap.get(key)[2] = avgGPA;
-                        scheduleMap.get(key)[3] = medianGPA;        
+                        scheduleMap.get(key)[3] = medianGPA;      
+                        scheduleMap.get(key)[4] = link;     
                     }
                 }       
             }
@@ -133,28 +138,49 @@ function processRequest(xhr, key) {
 
 /*
 Function that runs after all AJAX calls are complete. 
-Inserts the average gpa scraped from msu grades into the msu schedule builder website next to the appropriate professor name
+Calls injectHTML to inject the average and median gpa scraped from msu grades 
+into the msu schedule builder website next to the appropriate professor name
 */
 function insertHTML()
 {
     let i =0;
-    for (var value of scheduleMap.values()) 
+    for (var [key, value] of scheduleMap.entries()) 
     {
-        let avg_td  = document.createElement("td");
-        //Sets the inner HTML of the newly created td element to the average gpa from the scheduleMap
-        avg_td.innerHTML = value[2];
-        avg_td.id = "MainContent_UCEnrl_rptPlanner_tdAVGGPA" + i;
-        avg_td.className = "instructor-name";
-        let avgContentDiv = document.querySelector("#MainContent_UCEnrl_rptPlanner_trMeeting_" + i);
-        avgContentDiv.appendChild( avg_td);
-
-        let median_td  = document.createElement("td");
-        //Sets the inner HTML of the newly created td element to the median gpa from the scheduleMap
-        median_td.innerHTML = value[3];
-        median_td.id = "MainContent_UCEnrl_rptPlanner_tdMEDIANGPA" + i;
-        median_td.className = "instructor-name";
-        avgContentDiv.appendChild(median_td);
-        
+        injectHTML(i, value[2], value[4]);
+        injectHTML(i, value[3], value[4]);
         i++;
+    }
+}
+
+/*
+Helper function that injects html given a gpa (either average or median) and a link to the MSUGrades page.
+*/
+function injectHTML(i, gpa, link)
+{
+    //Creates a new td element for average/median grades with id and CSS class
+    let td  = document.createElement("td");
+    td.id = "MainContent_UCEnrl_rptPlanner_tdAVERAGEGPA" + i;
+    td.className = "instructor-name";   
+
+    //Locates the appropriate div to inject the td into in the HTML and appends it as a child 
+    let avgContentDiv = document.querySelector("#MainContent_UCEnrl_rptPlanner_trMeeting_" + i);
+    avgContentDiv.appendChild(td);
+
+    if(gpa != "N/A")
+    {
+        //Creates a new HTML a element
+        let a  = document.createElement("a");
+        //Sets the link target to be a new tab
+        a.target = "_blank";
+        //Sets the link to the baseURL of msugrades.com + the link for that specific course/professor
+        a.href =  BASEMSUGRADESURL + link; 
+        a.title = "Click to visit MSU Grades";
+        //Sets the inner HTML of the newly created td element to the gpa from the scheduleMap
+        a.innerText = gpa;   
+        td.appendChild(a);
+    }
+    else
+    {
+        td.innerText= gpa;
     }
 }
